9ea309a830b16a42abadaa1745527b5b49a89fac
erfat <erfatjahan456@gmail.com>
2026-09-10 19:27:56 +0600

    fix: optimize App.jsx routing and clean up useEffect hooks for Pay-For-Me feature

--- src/App.jsx
+++ src/App.jsx
@@ -25,24 +25,21 @@ import FAQ from "./pages/FAQ";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import PayForMe from "./pages/PayForMe";
+
 // Redux Actions
 import { getuser } from "./store/slices/authSlice";
 import { fetchAllProducts } from "./store/slices/productSlice";
 
 const App = () => {
   const dispatch = useDispatch();
-  const { authUser,isCheckingAuth } = useSelector((state) => state.auth);
+  const { authUser, isCheckingAuth } = useSelector((state) => state.auth);
 
-  useEffect(() => {
-    dispatch(getuser());
-  }, [getuser]);
-  
   useEffect(() => {
     dispatch(getuser());
     dispatch(fetchAllProducts());
   }, [dispatch]);
-const {products}=useSelector(state=>state.product)
-  if ((isCheckingAuth&&!authUser)||!products) {
+
+  if (isCheckingAuth && !authUser) {
     return (
       <div className="min-h-screen flex items-center justify-center bg-slate-50 dark:bg-[#120b0e]">
         <div className="w-10 h-10 border-4 border-[#9c5b6f]/30 border-t-[#9c5b6f] rounded-full animate-spin" />
