db39be25129618b783b74141a731a21b8c92b10c
erfat <erfatjahan456@gmail.com>
2026-09-14 20:42:30 +0600

    update function

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Star, ShoppingBag, Heart } from "lucide-react";
 import { Link } from "react-router-dom";
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../../store/slices/cartSlice";
 import { toggleCart } from "../../store/slices/popupSlice";
-import { addToWishlist, removeFromWishlist } from "../../store/slices/wishlistSlice";
+import { addToWishlist, removeFromWishlist } from "../../../store/slices/wishlistSlice";
 import { toast } from "react-toastify";
 
 const ProductCard = ({ product }) => {
@@ -129,7 +129,7 @@ const ProductCard = ({ product }) => {
             Price
           </span>
           <span className="text-base sm:text-lg font-extrabold text-[#9c5b6f] dark:text-[#e4a8b8]">
-            ${Number(product.price || 0).toFixed(2)}
+            ৳{Number(product.price || 0).toFixed(2)}
           </span>
         </div>
 
